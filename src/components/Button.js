223981205c86abc64df7f5c071a3535af37e1717
GidGid <gideonpeprah94@gmail.com>
2019-08-16 19:34:53 +0100

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const ButtonContainer = styled.button `
+text-transform: capitalize;
+font-size: 1.4rem;
+background: transparent;
+border: 0.05rem solid #009ffd;//var(--lightBlue);
+border-color: ${
+    props => props.cart ? "#ffa400" : "#009ffd"
+};
+color: ${props => props.cart? "#ffa400" : "#009ffd"};
+border-radius: 0.5rem;
+padding: 0.2rem 0.5rem;
+cursor: pointer;
+margin: 0.2rem 0.5rem 0.2rem 0;
+transition: all 0.5s ease-in-out;
+&:hover{
+    background: ${props => props.cart? "#ffa400" : "#009ffd"};
+    color: #2a2a72;//var(--mainBlue);
+}
+&:focus{
+    outline:none;
+}
+`;
