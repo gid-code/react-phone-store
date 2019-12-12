c7d4664eb08134044bce83753cbde3f974ee0472
gid-code <gideonpeprah94@gmail.com>
2019-12-12 12:08:09 +0000

    keys

--- src/config/keys_prod.js
+++ src/config/keys_prod.js
@@ -3,7 +3,7 @@ module.exports = {
         apiKey: process.env.API_KEY,
         authDomain: process.env.AUTH_DOMAIN,
         databaseURL: process.env.DATABASE_URL,
-        projectId: process.env.PROJECT_ID,
+        projectId: process.env.REACT_APP_PROJECT_ID,
         storageBucket: process.env.STORAGE_BUCKET,
         messagingSenderId: process.env.MESSAGING_SENDER_ID,
         appId: process.env.APP_ID,
